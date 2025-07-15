60961259154d3c9b707f9aa5f052c5799f05084b
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-16 02:31:27 +0300

    Create firebase_config.js

---
+++ firebase_config.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "nothing-ddb83",
+  "private_key_id": "f5722617d9e748b241d8f77e33d52360377e543c",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDJ0K5++rsPH+R1\nrz9k9MtLhbB+373E5yJYYnkDP1Tf3odLrE+vScBFFt+7OMuVyWJG9t3kDq7Fsz6i\nNh/RdnF5Q/lu4RhWGoir+WrPxjsOt+/58eccse59Yn7H7jbpLqZjD20qxqoyhFl4\nRWTNW4F7dcAsz5xE1gcZEUVERNPQHruCeHvs8IZebn2yW45HkHR6oHwNl2xYL86U\n92x5iYFLa/wIQ6ltGdCdhVspNqFtACGETb4XYinct/zVtP3O/dDcFKfINIdPq9CE\nFMBpb54T6GofJl9i/EfNpNDFhZmrwrPYsg9gj2RdOIVkNPPa1elN866ZsQFCWeIa\nc9DGxvmXAgMBAAECggEACFU3f0K3Co7Ujg9oXLDG3B1vBi4S1dh9UIzwVjP9Oykj\nCPraZ3ByIegp4L8dmNex2SBEVTBOAxe9lFELy0hKsykdWXit2pBJ6F/qCx4yPg9W\nN0nHsyWdpUu11Gd/mO02LYzrgwKk8GUKlK641MncdM6N2svPtf/oVx6hb4ov/L3V\nNQl2ufJeK1ZCfppBZgt2p8M88xj7qVRTT2NGwLY/JA0/TSAcFq7s7tvRSNE7Vo2O\ntarf7K9LbgPRY8yY4nC9dnsYhYmDlCQngrGX7/0iFS+9DfM8UdJ4aihRJgT5YCdR\nXepmVRMh2nFHJfSjzSH9WBTEk6rmbARGg6NNIS5iMQKBgQD/9m+rzudZpKgVyrMu\nEzaP8BMWSwF/6d2ZNPVoWOjsfq/6VPezu3lyMYpVjETLPpKi6rennzXA9ZaaHubZ\nFCe1xMBRyAOF4kqNo4eEhFATYjcx9ss8Hi16kfUKhDmgUUA1pDyHpkoFsNFvUAfD\nCnVsdPtQfs4yuZdvGo+ApM/jDQKBgQDJ2Djk/HE6kN2BXdetkAk5MOCVB9eyUG1k\n5Mb4NViBqVDJR+Gy0/dw/BZBJ2WeMiCOYKxz+v2qDl4tlSCS8Mvlzs86IIr2rMpO\nJkPzRzizJ5YjwKRWEOPIEKq7P8WA6OsryEDoFbWsGaiXYZVHWfOltcRF2FV1hOjO\n9qNXd1g2MwKBgHjtAmEXBPGy+r4g33obFaH29+2GThUSdeDVxkMgDhr810LbNZMy\nNPQgN4ynEjE7RKe3YEDO1ORuolH523IfNYuIgpugdRmhiTtVUMbog7REqHLwd5/V\nWgMYRb5MLbVx7XXqO7aSd1Is8SU3mBMrM8Kjmb+zyNLqH4Mb90P9mUFZAoGBAJHf\nDMcYVbBQr3Rl4JATj0bgtv2dbFn23AhVeiI5U1xpg/PjWg6GN8ZUuLSfh0X7ziF3\nZkpvctFqjs98fc0T7xeHDaZhjGxXVeGCsArqRuTa0BvIhjuY+zVELk8i+FaAfizn\nDVltDx1wcbHtmRe2v2nb4hvkC2biBepEvqlgm8SXAoGBAPtZ8eyuNfWuaCiD9lIW\nXR0N9XXWEZ0nALyFU8XP2ZSYihvPhdQpQETeWz/nlz3bjZB0d+CAIzpJns+XEN1z\nlbGqTPHzyvp85i7AzWFvgWo0pU4Wv95A5KV2mnigEh4gWhjBB23Lyil22fRWQDQ9\nDJfsERozWAqFEE7KYecRwTKo\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@nothing-ddb83.iam.gserviceaccount.com",
+  "client_id": "102263730028481148360",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40nothing-ddb83.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
