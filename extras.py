f0680da66e919673e6a9f9fb109f31406bf6d8e0
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-18 02:32:06 +0300

    Update extras.py

--- extras.py
+++ extras.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
 def extras_feature():
-    st.info("هذه ميزة إضافية يمكن تطويرها هنا بدون تعديل في orders.py أو run.py")
-
-def main():
-    # هنا تضع أي كود تريده يظهر في الموقع
-    st.header("قسم الإضافات")
-    extras_feature()
