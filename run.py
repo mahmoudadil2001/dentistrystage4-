7bc590a9b293eeced53935d7d970ff5330756d59
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-18 11:03:12 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 import streamlit as st
 from orders import main as orders_main
-import extras  # ربط ملف التنسيقات
+import extras
 
 def main():
-    # تطبيق التنسيقات أولاً
-    extras.apply_custom_styles()
-
-    # تشغيل التطبيق الرئيسي
-    orders_main()
+    extras.apply_custom_styles()  # طبق التنسيقات من ملف extras.py
+    orders_main()  # شغل التطبيق من ملف orders.py
 
 if __name__ == "__main__":
     main()
