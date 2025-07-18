04a0c21041f8083dcd2a669c7c223f4ea1d34c05
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-18 11:16:26 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-from orders import main
+import streamlit as st
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+def main():
+    local_css("styles.css")  # هنا تربط ملف الستايل
+    # باقي كود main...
+
+# مثال بسيط لتجربة
 if __name__ == "__main__":
     main()
