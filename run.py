779391d29caba93e201b066f3fe89e928486779f
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-18 10:50:49 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from orders import main as orders_main
-import extras  # ✅ ربط ملف التصميمات
+import extras  # ✅ ربط ملف التنسيقات
 
-# ✅ تطبيق التنسيقات
+# ✅ تحميل التنسيقات أولاً
 extras.load_custom_styles()
 
 # ✅ إظهار البطاقة الترحيبية (فقط إذا ما سجل اسمه)
 if "user_logged" not in st.session_state:
     extras.show_welcome_card()
 
-# ✅ تشغيل المنصة
+# ✅ تشغيل الموقع
 orders_main()
