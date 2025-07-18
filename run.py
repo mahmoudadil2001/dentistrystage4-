192276625276dbdcc51bb68e86480ef2adba0bcd
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-18 11:23:20 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,11 +4,6 @@ def local_css(file_name):
     with open(file_name, encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-def main():
-    local_css("styles.css")
-    st.button("زر تجريبي")  # جرب الزر مع التأثيرات
-    # باقي كودك...
-
 if __name__ == "__main__":
     main()
 from orders import main
