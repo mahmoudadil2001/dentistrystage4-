a97350926017fb51f4e5e81d2a013b4f83fddcba
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-18 11:23:50 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import streamlit as st
 def local_css(file_name):
     with open(file_name, encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
+def main():
+    local_css("styles.css")
+ 
 if __name__ == "__main__":
     main()
 from orders import main
