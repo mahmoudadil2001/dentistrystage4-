4c0b530368562c049f4875299a6aaaba863077cc
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-20 06:36:21 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,14 +3,11 @@ from orders import main as orders_main
 from chat import main as chat_main
 
 def main():
-    # ضبط القيمة الافتراضية لصفحة العرض (لو غير موجودة)
     if "page" not in st.session_state:
-        st.session_state.page = "orders"  # تبدأ بواجهة التسجيل
+        st.session_state.page = "orders"
 
-    # إذا الصفحة الحالية هي "orders" (تسجيل المستخدم)
     if st.session_state.page == "orders":
         orders_main()
-    # إذا الصفحة الحالية هي "chat" (الدردشة)
     elif st.session_state.page == "chat":
         chat_main()
 
