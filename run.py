01940aa8e16e8eec2185bf0679cb28718d1e2166
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-20 06:27:07 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,23 +1,15 @@
 import streamlit as st
 from orders import main as orders_main
-from chat import main as chat_main  # استيراد من chat.py بدل main.py
+from chat import main as chat_main
 
 if "page" not in st.session_state:
-    st.session_state.page = "orders"  # البداية بالصفحة الرئيسية (orders)
+    st.session_state.page = "orders"
 
 def run_app():
     if st.session_state.page == "orders":
         orders_main()
     elif st.session_state.page == "chat":
         chat_main()
-
-if __name__ == "__main__":
-    run_app()
-import streamlit as st
-from chat import main as chat_main
-
-def run_app():
-    chat_main()
 
 if __name__ == "__main__":
     run_app()
