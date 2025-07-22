b39c01277acabe81c04fc7813cae973b8c17ccff
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-22 03:53:46 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import streamlit as st
 from orders import main as orders_main
 from chat import main as chat_main
+from welcome import main as welcome_main
 
 def main():
     if "page" not in st.session_state:
-        st.session_state.page = "orders"
+        st.session_state.page = "welcome"
 
-    if st.session_state.page == "orders":
+    if st.session_state.page == "welcome":
+        welcome_main()
+    elif st.session_state.page == "orders":
         orders_main()
     elif st.session_state.page == "chat":
         chat_main()
