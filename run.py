94f6beccfc3c997b7561b5d3fab6a51706327d9d
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-22 04:18:38 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from orders import main as orders_main
 
+def load_css(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        css = f.read()
+    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+
 def main():
+    load_css("styles.css")  # make sure styles.css is in the same folder as run.py
     orders_main()
 
 if __name__ == "__main__":
