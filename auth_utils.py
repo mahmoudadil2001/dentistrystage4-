62caf99ca8c85a1fec781c012c9923eae3b98c9d
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-23 05:23:54 +0300

    Delete auth_utils.py

--- auth_utils.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-def hash_password(password_plain):
-    password_bytes = password_plain.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password_bytes, salt)
-    return hashed.decode('utf-8')
