fccbf804c2acece9fc91f47e4e8d328c1679af3b
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-23 05:07:27 +0300

    Update auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -1,7 +1,6 @@
 import streamlit_authenticator as stauth
 
-def hash_password(password_plain: str) -> str:
-    # دالة تستقبل كلمة سر نصية وتعيد هاش مشفر
-    hashed_list = stauth.Hasher([password_plain]).generate()
-    hashed = hashed_list[0]  # نأخذ أول عنصر من القائمة لأننا مررنا كلمة واحدة فقط
+def hash_password(password_plain):
+    # يحمّل قائمة كلمة سر واحدة فقط ويُرجع الهاش كقيمة نصية
+    hashed = stauth.Hasher([password_plain]).generate()[0]
     return hashed
