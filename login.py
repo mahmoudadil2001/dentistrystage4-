4b5633eaa5569251a530f7cda0c62e998dd320aa
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-28 11:36:25 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-token = st.secrets[""]
+token = st.secrets["1"]
 headers = {"Authorization": f"token {token}"} if token else {}
 
 def get_user_repos(username):
