c1c25a8947966f1708d4a27d441291deee468dc0
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-29 15:17:52 +0300

    Update orders.py

--- orders.py
+++ orders.py
@@ -4,7 +4,7 @@ import importlib.util
 import sys
 import importlib
 
-from versions_manager import get_lectures_and_versions, select_version_ui
+from versions_manager import get_lectures_and_versions, select_version_ui_with_checkboxes
 
 
 def load_lecture_titles(subject_name):
@@ -73,7 +73,8 @@ def orders_o():
 
     versions_dict = lectures_versions.get(lec_num, {})
 
-    selected_version = select_version_ui(versions_dict)
+    # استخدم الدالة الجديدة لواجهة اختيار النسخة مع checkboxes
+    selected_version = select_version_ui_with_checkboxes(versions_dict)
 
     filename = versions_dict[selected_version]
     file_path = os.path.join(subject, filename)
@@ -230,16 +231,4 @@ def main():
             Telegram Channel
             <span style="width:24px; height:24px; background:#fff; border-radius:50%; display:flex; justify-content:center; align-items:center; margin-left:8px;">
                 <svg viewBox="0 0 240 240" xmlns="http://www.w3.org/2000/svg" style="width:16px; height:16px; fill:#0088cc;">
-                    <path d="M120 0C53.7 0 0 53.7 0 120s53.7 120 120 120 120-53.7 120-120S186.3 0 120 0zm58 84.6l-19.7 92.8c-1.5 6.7-5.5 8.4-11.1 5.2l-30.8-22.7-14.9 14.3c-1.7 1.7-3.1 3.1-6.4 3.1l2.3-32.5 59.1-53.3c2.6-2.3-.6-3.6-4-1.3l-72.8 45.7-31.4-9.8c-6.8-2.1-6.9-6.8 1.4-10.1l123.1-47.5c5.7-2.2 10.7 1.3 8.8 10z"/>
-                </svg>
-            </span>
-        </a>
-    </div>
-
-    <div style="text-align:center; margin-top:15px; font-size:16px; color:#444;">
-        Subscribe to the Telegram channel to get all updates and new lectures I will upload here, God willing.
-    </div>
-    ''', unsafe_allow_html=True)
-
-if __name__ == "__main__":
-    main()
+                    <path d="M120 0C53.7 0 0 53.7 0 120s53.7 120 120 120 120-53.7 120-120S186.3 0 120 0zm58 84.6l-19.7 92.8c-1.5 6.7-5.5 8.4-11.1 5.2l-30.8-22.7-14.9 14.3c-1.7 1.7-3.1 3.1-6.4 3.1l2.3-32.5 59.1-53.3c2.6-2.3-.6-3.6-4-1.3l-72.8 45.7-31.4-9.8c-6.8-2.1-6.9-6.8 1.4-10
