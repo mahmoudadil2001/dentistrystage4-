5fa63babc1b160e7a59211fc643cef5711f7a05b
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-31 03:09:24 +0300

    Update orders3.py

--- orders3.py
+++ orders3.py
@@ -1,18 +1,19 @@
 import streamlit as st
-from orders2 import normalize_answer   # أضف هذا السطر
+from orders2 import normalize_answer
 
 def add_sidebar_navigation(subject, questions):
-    st.sidebar.markdown(f"### 🧪 {subject.upper()}")
-    for i in range(len(questions)):
-        correct_text = normalize_answer(questions[i])
-        user_ans = st.session_state.user_answers[i]
-        if user_ans is None:
-            status = "⬜"
-        elif user_ans == correct_text:
-            status = "✅"
-        else:
-            status = "❌"
+    with st.sidebar:
+        st.markdown(f"### 🧪 {subject.upper()}")
 
-        if st.sidebar.button(f"{status} Question {i+1}", key=f"nav_{i}"):
-            st.session_state.current_question = i
-            st.rerun()
+        for i in range(len(questions)):
+            correct_text = normalize_answer(questions[i])
+            user_ans = st.session_state.user_answers[i]
+            if user_ans is None:
+                status = "⬜"
+            elif user_ans == correct_text:
+                status = "✅"
+            else:
+                status = "❌"
+
+            if st.button(f"{status} Question {i+1}", key=f"nav_{i}"):
+                st.session_state.current_question = i
