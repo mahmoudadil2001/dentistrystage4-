56236d18901a7e30b80c4bda773bba33cf59f801
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-31 03:09:40 +0300

    Update orders.py

--- orders.py
+++ orders.py
@@ -1,18 +1,16 @@
 import streamlit as st
 from orders1 import load_and_select_subject_lecture_version
-from orders2 import normalize_answer, show_question, show_quiz_summary
+from orders2 import show_question, show_quiz_summary
 from orders3 import add_sidebar_navigation
 
 def main():
-    # تحميل الأسئلة والمحاضرات والإصدارات
     subject, lec_num, selected_version, data = load_and_select_subject_lecture_version()
 
     if subject is None:
-        return  # خطأ أو لا توجد بيانات
+        return
 
     questions, Links = data
 
-    # إعادة تهيئة الحالة عند تغير المحاضرة أو الموضوع أو النسخة
     if ("questions_count" not in st.session_state) or \
        (st.session_state.questions_count != len(questions)) or \
        (st.session_state.get("current_lecture", None) != lec_num) or \
@@ -35,7 +33,6 @@ def main():
     else:
         show_quiz_summary(questions)
 
-    # عرض الروابط الإضافية
     if Links:
         st.markdown("---")
         for link in Links:
