1585ab17a02de4f478c02ca551355d54750052ef
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-31 04:44:28 +0300

    Update orders.py

--- orders.py
+++ orders.py
@@ -34,17 +34,14 @@ def import_module_from_file(filepath):
 
 
 def orders_o():
-    # زر التبديل بين إظهار/إخفاء الواجهة
     if "show_quiz_controls" not in st.session_state:
         st.session_state.show_quiz_controls = True
 
-col1, col2, col3 = st.columns([1,2,1])
-with col2:
-    
-    if st.button("Toggle Quiz Controls"):
-        st.session_state.show_quiz_controls = not st.session_state.show_quiz_controls
-        st.rerun()
-
+    col1, col2, col3 = st.columns([1,2,1])
+    with col2:
+        if st.button("Toggle Quiz Controls"):
+            st.session_state.show_quiz_controls = not st.session_state.show_quiz_controls
+            st.rerun()
 
     if st.session_state.show_quiz_controls:
         st.markdown(
